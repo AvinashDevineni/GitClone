b07c966a83c711838d9313039172e196916f7923
Avinash Devineni <devinenia69420@gmail.com>
2024-12-10 20:38:09 -0600

    Added ignore file creation and addition by command line and fixed issue with added file paths creation

--- gitclone.cpp
+++ gitclone.cpp
@@ -2,6 +2,7 @@
 #include <fstream>
 #include <filesystem>
 #include <sys/stat.h>
+#include <cstring>
 
 const std::string name = "gitclone";
 const std::string version = "0.0.1";
@@ -23,7 +24,7 @@ const std::string version = "0.0.1";
 //     return line;
 // }
 
-bool isCwdInitialized(std::string& cwd) {
+bool isCwdInitialized(const std::string& cwd) {
     struct stat _;
     std::string dataPath = cwd + "/." + name;
     int res = stat(dataPath.c_str(), &_);
@@ -37,37 +38,70 @@ int main(int argc, char ** argv) {
         return 0;
     }
 
-    char* cmd = argv[1];
-    std::string cwd = std::filesystem::current_path().string();
-    std::string dataPath = cwd + "/." + name;
+    const char* CMD = argv[1];
+    const std::string CWD = std::filesystem::current_path().string();
+    const std::string DATA_PATH = CWD + "\\." + name;
 
-    if (strcmp("init", cmd) == 0)
-        std::filesystem::create_directory(dataPath);
+    if (strcmp("init", CMD) == 0)
+        std::filesystem::create_directory(DATA_PATH);
 
-    else if (strcmp("add", cmd) == 0) {
-        if (!isCwdInitialized(cwd)) {
-            std::cout << "Repository is not initialized. You probably want to run \"init\"";
+    else if (strcmp("add", CMD) == 0) {
+        if (!isCwdInitialized(CWD)) {
+            std::cerr << "Repository is not initialized. You probably want to run \"init\"";
             return 0;
         }
 
-        const std::string addPath = dataPath + "/add.txt";
+        const std::string ADD_PATH = DATA_PATH + "/addpaths.txt";
         std::ofstream addFile;
 
         // initializing add file to write or append
-        // depending on if it already exists
+        // depending on if we are adding everything
+        // or if the file already exists or doesn't exist
         struct stat _;
-        if (!stat(dataPath.c_str(), &_))
-            addFile.open(addPath);
-        else addFile.open(addPath, std::ios_base::app);
+        if ((argc == 2 || strcmp(argv[2], "all")) || stat(ADD_PATH.c_str(), &_) != 0)
+            addFile.open(ADD_PATH);
+        else addFile.open(ADD_PATH, std::ios_base::app);
 
 
         // adding all files to add file
         if (argc == 2 || strcmp(argv[2], "all")) {
-            for (const auto& entry : std::filesystem::recursive_directory_iterator(cwd)) {
+            for (const auto& entry : std::filesystem::recursive_directory_iterator(CWD)) {
                 if (entry.is_directory())
                     continue;
-                addFile << entry.path().string() << std::endl;
+                
+                const std::string PATH = entry.path().string();
+                // PATH starts with DATA_PATH
+                if (PATH.find(DATA_PATH) == 0)
+                    continue;
+
+                addFile << PATH << std::endl;
             }
         }
+    }
+
+    else if (strcmp("ignore", CMD) == 0) {
+        if (argc == 2) {
+            std::cerr << "File/folder to ignore must be given.";
+            return 1;
+        }
+
+        if (strchr(argv[2], '/')) {
+            std::cerr << "For subdirectories, use \"\\\" instead of \"/\"";
+            return 1;
+        }
+
+        const std::string IGNORE_PATH = CWD + "/.ignore.txt";
+        std::ofstream ignoreFile;
+
+        struct stat _;
+        int res = stat(IGNORE_PATH.c_str(), &_);
+        if (res)
+            std::cout << res;
+        if (stat(IGNORE_PATH.c_str(), &_) != 0)
+            ignoreFile.open(IGNORE_PATH);
+        else ignoreFile.open(IGNORE_PATH, std::ios_base::app);
+
+        ignoreFile << argv[2] << "\n";
+        ignoreFile.close();
     }
 }
