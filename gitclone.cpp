129d4ebdaf19e35c8907d262053388e5b8eda40d
Avinash Devineni <devinenia69420@gmail.com>
2024-12-12 15:01:39 -0600

    Made addpaths file have relative paths and implemented skeleton of save command that creates a save file and outputs the added files

--- gitclone.cpp
+++ gitclone.cpp
@@ -6,31 +6,13 @@
 #include <algorithm>
 #include <unordered_set>
 #include <functional>
+#include <chrono>
 
-const char* name = "gitclone";
-const char* version = "0.0.1";
+constexpr char* name = "gitclone";
+constexpr char* version = "0.0.1";
 
-const char* ignoreFileName = ".ignore.txt";
-
-// std::string readFile(const char* path) {
-//     std::ifstream file(path);
-
-//     if (!file) { // Check if the file was opened successfully
-//         std::cerr << "Error: Could not open file " << filename << std::endl;
-//         return 1;
-//     }
-
-//     std::string line;
-//     while (std::getline(file, line)) { // Read the file line by line
-//         std::cout << line << std::endl; // Print each line
-//     }
-
-//     file.close(); // Close the file
-//     return line;
-// }
-
-std::unordered_set<std::string> tryReadIgnoreFile(const std::string& CWD) {
-    std::ifstream ignoreFile(CWD + "\\" + ignoreFileName);
+std::unordered_set<std::string> tryReadFile(const std::string& PATH) {
+    std::ifstream ignoreFile(PATH);
     if (!ignoreFile)
         return {};
 
@@ -44,16 +26,18 @@ std::unordered_set<std::string> tryReadIgnoreFile(const std::string& CWD) {
 }
 
 void addFilePathsInDirToFolder(const std::string* dirPath, std::ofstream& outFile,
-                               std::function<bool(std::string&)> shouldIgnoreFile) {
+                               std::function<bool(std::string&)> shouldIgnoreFile,
+                               std::function<std::string(std::filesystem::path&)> getPathStr) {
     for (const auto& entry : std::filesystem::recursive_directory_iterator(*dirPath)) {
         if (entry.is_directory())
             continue;
         
-        std::string path = entry.path().string();
-        if (shouldIgnoreFile(path))
+        auto path = entry.path();
+        std::string pathStr = getPathStr(path);
+        if (shouldIgnoreFile(pathStr))
             continue;
 
-        outFile << path << "\n";
+        outFile << pathStr << "\n";
     }
 }
 
@@ -69,53 +53,68 @@ int main(int argc, char ** argv) {
         return 0;
     }
 
-    const char* CMD = argv[1];
     const std::string CWD = std::filesystem::current_path().string();
-    const std::string DATA_PATH = CWD + "\\." + name;
 
-    if (strcmp("init", CMD) == 0)
-        std::filesystem::create_directory(DATA_PATH);
+    const std::string DATA_PATH_REL = std::string("\\.") + name;
+    const std::string ADD_PATH_REL = DATA_PATH_REL + "\\addpaths.txt";
+    const std::string IGNORE_PATH_REL = "\\.ignore.txt";
+    const std::string SAVES_PATH_REL = DATA_PATH_REL + "\\" + "saves";
 
-    else if (strcmp("add", CMD) == 0) {
-        if (!isCwdInitialized(CWD)) {
-            std::cerr << "Repository is not initialized. You probably want to run \"init\"";
-            return 1;
-        }
+    const std::string DATA_PATH_ABS = CWD + DATA_PATH_REL;
+    const std::string ADD_PATH_ABS = CWD + ADD_PATH_REL;
+    const std::string IGNORE_PATH_ABS = CWD + IGNORE_PATH_REL;
+    const std::string SAVES_PATH_ABS = DATA_PATH_ABS + "\\" + "saves";
 
-        const std::string ADD_PATH = DATA_PATH + "\\addpaths.txt";
+    const char* CMD = argv[1];
+
+    // commands that don't need repo to be initialized
+    if (strcmp("init", CMD) == 0) {
+        std::filesystem::create_directory(DATA_PATH_REL);
+        return 0;
+    }
+
+    if (!isCwdInitialized(CWD)) {
+        std::cerr << "Repository is not initialized. You probably want to run \"init\"";
+        return 1;
+    }
+
+    // commands that need repo to be initialized
+    if (strcmp("add", CMD) == 0) {
         const bool SHOULD_ADD_ALL = argc == 2 || strcmp(argv[2], "all") == 0;
         std::ofstream addFile;
 
         // initializing add file to write or append
         // depending on if we are adding everything
         // or if the file already exists or doesn't exist
-        struct stat _;
-        if (SHOULD_ADD_ALL || !std::filesystem::exists(ADD_PATH.c_str()))
-            addFile.open(ADD_PATH);
-        else addFile.open(ADD_PATH, std::ios_base::app);
+        if (SHOULD_ADD_ALL || !std::filesystem::exists(ADD_PATH_ABS.c_str()))
+            addFile.open(ADD_PATH_ABS);
+        else addFile.open(ADD_PATH_ABS, std::ios_base::app);
 
-        const auto ignoredPaths = tryReadIgnoreFile(CWD);
+        const auto ignoredPaths = tryReadFile(IGNORE_PATH_ABS);
 
-        // adding all files to add file
+        // handling adding all files to add file
         if (SHOULD_ADD_ALL) {
-            addFilePathsInDirToFolder(&CWD, addFile, [DATA_PATH, ignoredPaths, CWD](std::string& PATH) -> bool{
+            addFilePathsInDirToFolder(&CWD, addFile, [DATA_PATH_REL, ignoredPaths, CWD](std::string& PATH) -> bool{
                 // PATH starts with DATA_PATH
-                if (PATH.find(DATA_PATH) == 0)
+                if (PATH.find(DATA_PATH_REL) == 0)
                     return true;
 
                 bool isIgnored = false;
                 for (const auto& ignoredPath : ignoredPaths) {
-                    if (PATH.find(CWD + "\\" + ignoredPath) == 0) {
+                    if (PATH.find(ignoredPath) == 0) {
                         isIgnored = true;
                         return true;
                     }
                 }
 
                 return false;
+            }, [CWD](std::filesystem::path& PATH) -> std::string{
+                return PATH.lexically_proximate(CWD).string();
             });
         }
 
         else {
+            // is this needed?
             for (int i = 0; argv[2][i] != '\0'; i++) {
                 if (argv[2][i] == '/')
                     argv[2][i] = '\\';
@@ -134,26 +133,28 @@ int main(int argc, char ** argv) {
                 return 1;
             }
 
-            const std::string ignorePath = CWD + "\\" + argv[2];
-            if (std::filesystem::is_directory(ignorePath)) {
-                addFilePathsInDirToFolder(&ignorePath, addFile, [DATA_PATH, ignoredPaths, CWD](std::string& PATH) -> bool{
+            const std::string addPath = CWD + "\\" + argv[2];
+            if (std::filesystem::is_directory(addPath)) {
+                addFilePathsInDirToFolder(&addPath, addFile, [DATA_PATH_REL, ignoredPaths, CWD](std::string& PATH) -> bool{
                     // PATH starts with DATA_PATH
-                    if (PATH.find(DATA_PATH) == 0)
+                    if (PATH.find(DATA_PATH_REL) == 0)
                         return true;
 
                     bool isIgnored = false;
                     for (const auto& ignoredPath : ignoredPaths) {
-                        if (PATH.find(CWD + "\\" + ignoredPath) == 0) {
+                        if (PATH.find(ignoredPath) == 0) {
                             isIgnored = true;
                             return true;
                         }
                     }
 
                     return false;
+                }, [CWD](std::filesystem::path& PATH) -> std::string{
+                    return PATH.lexically_proximate(CWD).string();
                 });
             }
 
-            else addFile << CWD + "\\" + argv[2] << "\n";
+            else addFile << argv[2] << "\n";
         }
     }
 
@@ -168,15 +169,43 @@ int main(int argc, char ** argv) {
                 argv[2][i] = '\\';
         }
 
-        const std::string IGNORE_PATH = CWD + "\\" + ignoreFileName;
         std::ofstream ignoreFile;
 
-        struct stat _;
-        if (!std::filesystem::exists(IGNORE_PATH.c_str()))
-            ignoreFile.open(IGNORE_PATH);
-        else ignoreFile.open(IGNORE_PATH, std::ios_base::app);
+        if (!std::filesystem::exists(IGNORE_PATH_ABS.c_str()))
+            ignoreFile.open(IGNORE_PATH_ABS);
+        else ignoreFile.open(IGNORE_PATH_ABS, std::ios_base::app);
 
         ignoreFile << argv[2] << "\n";
         ignoreFile.close();
+    }
+
+    else if (strcmp("save", CMD) == 0) {
+        if (argc == 2) {
+            std::cerr << "Message must be specified in quotes after \"save\" command";
+            return 1;
+        }
+
+        // 1. create saves folder if not present
+        // 2. create folder with number representing current save
+        // 3. add added files to folder
+
+        if (!std::filesystem::exists(SAVES_PATH_ABS.c_str()))
+            std::filesystem::create_directory(SAVES_PATH_ABS);
+
+        const auto time = std::chrono::system_clock::now();
+        const std::string SAVE_PATH_ABS = SAVES_PATH_ABS + "\\" + std::to_string(std::chrono::duration_cast<std::chrono::milliseconds>(time.time_since_epoch()).count());
+        std::filesystem::create_directory(SAVE_PATH_ABS);
+
+        const auto CWD_LEN = CWD.length();
+        const auto addedPaths = tryReadFile(ADD_PATH_ABS);
+        std::ofstream test(SAVE_PATH_ABS + "\\" + "test.txt");
+        for (const auto& addedPath : addedPaths) {
+            test << addedPath << "\n";
+        }
+    }
+
+    else {
+        std::cerr << "Command not recognized";
+        return 1;
     }
 }
